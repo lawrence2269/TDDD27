d9166751ec316066c7df80e4d2996e37dbeb1ba5
Lawrence Thanakumar <lawrence@Lawrences-MacBook-Pro.local>
2020-07-19 03:12:22 +0200

    For deployment, created an express server

---
+++ Client/SWMDB/server.js
@@ -0,0 +1,20 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/SWMDB'));
+console.log(__dirname);
+ 
+app.get('/*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/SWMDB/index.html'));
+});
+// Start the app by listening on the default Heroku port
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+  console.log("App is listening on "+port+ '.');
+});
